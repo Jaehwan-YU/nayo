3f680d9662579f162a4cda92e51e004eeb0dfbc0
sora <sorastar31@gmail.com>
2018-06-27 16:43:12 +0900

    [함수 테스트]

--- src/com/nayo/web/member/ShopbagService.java
+++ src/com/nayo/web/member/ShopbagService.java
@@ -1,7 +1,52 @@
 package com.nayo.web.member;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ShopbagService {
-	void getShopbagList(String memberEmail) {}
+	private String name;
+	private String regEmail;
+	private String buyDay;
+	
+	public void ShopbagService() {
+		this.name = null;
+		this.regEmail = null;
+		this.buyDay = null;
+	}
+	public void ShopbagService(String regEmail) {
+		this.regEmail = regEmail;
+	}
+	
+	void getShopbagList(){
+		
+	}
 	void addShopbag(String memberEmail) {}
 	void deleteShopbag(String shopbagId) {}
+	
+	public void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRegEmail() {
+		return regEmail;
+	}
+	public void setRegEmail(String regEmail) {
+		this.regEmail = regEmail;
+	}
+	public String getBuyDay() {
+		return buyDay;
+	}
+	public void setBuyDay(String buyDay) {
+		this.buyDay = buyDay;
+	}
 }
