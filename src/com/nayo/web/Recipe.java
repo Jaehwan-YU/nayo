5b195ed937d1379e494dc11e2c11d473ad280ea7
son_s <son_s@son>
2018-06-28 18:20:29 +0900

    Recipe
    관련 서비스 추가

---
+++ src/com/nayo/web/Recipe.java
@@ -0,0 +1,91 @@
+package com.nayo.web;
+
+public class Recipe {
+		
+	private String id;
+	private String title;
+	private String simpleIntro;
+	private String process;
+	private String regEmail;
+	private int kcalory;
+	private String national;
+	private String situation;
+	private String recipeType;
+	
+	
+	public Recipe() {
+		
+	}
+	
+	public Recipe(String id, String title, String simpleIntro, String process, String regEmail, int kcalory,
+			String national, String situation, String recipeType) {
+		
+		this.id = id;
+		this.title = title;
+		this.simpleIntro = simpleIntro;
+		this.process = process;
+		this.regEmail = regEmail;
+		this.kcalory = kcalory;
+		this.national = national;
+		this.situation = situation;
+		this.recipeType = recipeType;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSimpleIntro() {
+		return simpleIntro;
+	}
+	public void setSimpleIntro(String simpleIntro) {
+		this.simpleIntro = simpleIntro;
+	}
+	public String getProcess() {
+		return process;
+	}
+	public void setProcess(String process) {
+		this.process = process;
+	}
+	public int getKcalory() {
+		return kcalory;
+	}
+	public void setKcalory(int kcalory) {
+		this.kcalory = kcalory;
+	}
+	public String getRegEmail() {
+		return regEmail;
+	}
+	public void setRegEmail(String regEmail) {
+		this.regEmail = regEmail;
+	}
+	public String getNational() {
+		return national;
+	}
+	public void setNational(String national) {
+		this.national = national;
+	}
+	public String getSituation() {
+		return situation;
+	}
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+	public String getRecipeType() {
+		return recipeType;
+	}
+	public void setRecipeType(String recipeType) {
+		this.recipeType = recipeType;
+	}
+	
+	
+	
+}
--- src/com/nayo/web/RecipeService.java
+++ src/com/nayo/web/RecipeService.java
@@ -1,6 +1,108 @@
 package com.nayo.web;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecipeService {
-	//void getRecipeList() 
+
+	private List<Recipe> recipeList;
 	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		RecipeService test = new RecipeService();
+		
+		test.recipeList = new ArrayList<Recipe>();
+		
+		test.getRecipeList();
+		
+		test.printRecipeList();
+		
+	}
+	
+	void RecipeService() {
+		
+	}
+	
+
+	void getRecipeList() throws SQLException, ClassNotFoundException {
+		
+		String url = "jdbc:oracle:thin:@211.238.142.251:1521:orcl";
+		String user = "c##nayoadmin";
+		String password = "skdy0514";
+		
+		String sql = "SELECT R.ID 게시번호"
+				+", TITLE 음식명"
+				+", SIMPLE_INTRO 한줄소개"
+				+", PROCESS 조리과정"
+				+", REG_EMAIL 등록자"
+				+", KCALORY 열량"
+				+", N.NAME 국가"
+				+", S.NAME 상황"
+				+", T.NAME 종류 "
+				+"FROM RECIPE R "
+				+"INNER JOIN NATIONAL N "
+				+"ON N.ID = R.NATIONAL_ID "
+				+"INNER JOIN SITUATION S "
+				+"ON S.ID = R.SITUATION_ID "
+				+"INNER JOIN RECIPE_TYPE T "
+				+"ON T.ID = R.RECIPE_TYPE_ID";
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+	
+		while(rs.next()) {
+		
+		Recipe temp = new Recipe(
+				rs.getString("게시번호"),
+				rs.getString("음식명"),
+				rs.getString("한줄소개"), 
+				rs.getString("조리과정"),
+				rs.getString("등록자"),
+				rs.getInt("열량"),
+				rs.getString("국가"),
+				rs.getString("상황"), 
+				rs.getString("종류")
+				);
+	
+		
+		this.recipeList.add(temp);
+		
+		}
+	}
+	
+	void getRecipe() {
+		
+	}
+	
+	void addRecipe() {
+		
+	}
+	
+	void setRecipe() {
+		
+	}
+	
+	void printRecipeList() {
+		
+		for(int i=0; i<this.recipeList.size();i++) {
+		System.out.print(this.recipeList.get(i).getId()
+				+this.recipeList.get(i).getTitle()
+				+this.recipeList.get(i).getSimpleIntro()
+				+this.recipeList.get(i).getKcalory()
+				+this.recipeList.get(i).getNational()
+				+this.recipeList.get(i).getSituation()
+				+this.recipeList.get(i).getRecipeType()
+				+this.recipeList.get(i).getRegEmail()
+				+this.recipeList.get(i).getProcess()
+				);
+		}
+	}	
 }
