549a43f3763727453b0ce34d9e7d8425a8dcdbb7
sist28 <sist28@sist-28>
2018-06-28 15:38:31 +0900

    food

--- src/com/nayo/web/member/BookmarkService.java
+++ src/com/nayo/web/member/BookmarkService.java
@@ -1,7 +1,20 @@
 package com.nayo.web.member;
 
 public class BookmarkService {
+	
+	private String Recipe;
+	private String 
+	
+	
+	
 	void getBookMarkList() {}
+	
+	
+	
 	void addBookMark(String memberEmail, String RecipeId) {}
+	
+	
+	
+	
 	void deleteBookMark(String memberEmail, String RecipeId) {}
 }
