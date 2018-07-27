dd7b4888973b20498e81d497bb5407cab9b29ee8
user <user@user-pc>
2018-07-27 15:18:11 +0900

    대표는 머리가 좋다

---
+++ WebContent/js/shopbag.js
@@ -0,0 +1,15 @@
+
+window.addEventListener("load", function(){
+
+    var vegbutton = document.getElementById("veg_button");	 
+     
+    vegbutton.onclick(function() { 
+
+        alert("aa");
+
+        window.open('','','scrollbars=yes,toolbar=yes,resizable=yes,width=100,height=100,left=0,top=0');
+    
+    
+     } , 800);    
+ });
+                        
